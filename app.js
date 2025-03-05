d6fb1c5b80502b2863d03c0b20263684bda6de71
Ngure Kibaara <ngurekibaara01@gmail.com>
2025-03-06 02:41:18 +0300

    Clean Up app.js

--- app.js
+++ app.js
@@ -10,15 +10,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventsRouter = require('./routes/eventsRouter'); // Import events routes
 const newsRouter = require('./routes/newsRouter');
+const discussionRoutes = require('./routes/discussionRoutes');
+const mentorshipRoutes = require('./routes/mentorshipRoutes');
+
+
 
 var app = express();
 
-const url = 'mongodb://127.0.0.1:27017/moringaconnect';
-const connect = mongoose.connect(url, {});
+const url = "mongodb://127.0.0.1:27017/moringaconnect";
+mongoose.connect(url, {})
+  .then(() => console.log("Connected correctly to server"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
 
-connect.then(() => console.log('Connected correctly to server'),
-  err => console.log(err)
-);
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -32,8 +35,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/events', eventsRouter); // Add the events API
-app.use("/news", newsRouter);
+
+// Add the events API
+app.use('/api/events', eventsRouter); 
+app.use("/api/news", newsRouter);
+app.use('/api/discussions', discussionRoutes);
+app.use('/api/mentorship', mentorshipRoutes);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
